db5d674a2a8e5f8e11c675bc94b404da676af2cf
Gustavo Graciano <66232093+gustahh@users.noreply.github.com>
2025-10-06 17:54:45 -0300

    🚮remove: rota para criar quote

--- src/modules/quotes/quotes.controller.ts
+++ src/modules/quotes/quotes.controller.ts
@@ -7,19 +7,6 @@ import { ApiTags, ApiOperation } from '@nestjs/swagger';
 export class QuotesController {
   constructor(private readonly quotesService: QuotesService) {}
 
-  @Get('daily')
-  @ApiOperation({ summary: 'Returns a daily quote and save it in the database.' })
-  async getDailyQuotes() {
-    const quote = await this.quotesService.getTodayQuote();
-
-    const response: any = { original: { text: quote.text, author: quote.author } };
-    for (const t of quote.translations) {
-      response[t.language.toLowerCase()] = { text: t.text, author: quote.author };
-    }
-
-    return response;
-  }
-
   @Get()
   @ApiOperation({ summary: 'Returns all saved quotes.' })
   async getAllQuotes() {
