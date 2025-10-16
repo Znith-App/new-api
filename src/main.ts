4f3a28f27f368c0270716d8b6ff3b49c0aa32004
Gustavo Graciano <66232093+gustahh@users.noreply.github.com>
2025-10-16 11:27:13 -0300

    ✨feat: cors para desenvolvimento

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,12 @@ async function bootstrap() {
   try {
     const app = await NestFactory.create(AppModule);
 
+    app.enableCors({
+      origin: ['http://localhost:8081'],
+      methods: ['GET', 'POST', 'PUT', 'DELETE'],
+      credentials: true,
+    });
+
     const config = new DocumentBuilder()
       .setTitle('Zenith API')
       .setDescription('API do Zenith.')
